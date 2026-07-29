8b898f9fe9af999ac1fdbfd797970f9e4463473c
Chetoh16 <egecetin1511@gmail.com>
2026-07-29 19:08:35 +0100

    feat: calculate pspi (pain) score

--- facial_landmark_tracker/landmark_motion.py
+++ facial_landmark_tracker/landmark_motion.py
@@ -30,7 +30,37 @@ PAIN_RELEVANT_BLENDSHAPES = [
     "eyeBlinkLeft", "eyeBlinkRight",
 ]
 
+def calculate_pspi(blendshape_scores):
+    """
+    Prkachin & Solomon Pain Intensity (PSPI) is used to classify four levels of pain intensity (none, trace, weak, and strong).
 
+    Approximate the PSPI score:
+        PSPI = AU4 + max(AU6, AU7) + max(AU9, AU10) + AU43
+
+    blendshape_scores: dict of {category_name: score} for ONE frame.
+    Returns a single float - higher means MORE PAIN
+
+    The real formula uses actual AU intensities (like from py-feat/OpenFace)
+    not blendshapes (which are not true AUs) so this is just a demo / proof of concept. 
+    """
+
+    # helper function to fetch scores with a default fallback of 0.0
+    def get_score(name):
+        return blendshape_scores.get(name, 0.0)
+
+    au4 = max(get_score("browDownLeft"), get_score("browDownRight"))
+    au6_7 = max(get_score("eyeSquintLeft"), get_score("eyeSquintRight"))  #6 7
+    au9_10 = max(
+        get_score("noseSneerLeft"), 
+        get_score("noseSneerRight"),
+        get_score("mouthUpperUpLeft"), 
+        get_score("mouthUpperUpRight")
+    )
+    au43 = max(get_score("eyeBlinkLeft"), get_score("eyeBlinkRight"))
+
+    pspi_score = au4 + au6_7 + au9_10 + au43
+
+    return pspi_score
 
 # configure landmarker for VIDEO mode
 options = FaceLandmarkerOptions(
@@ -81,13 +111,16 @@ with FaceLandmarker.create_from_options(options) as landmarker:
                     # OpenCV uses BGR instead of RGB (sure go be unique)
                     cv2.circle(frame, (cx,cy), 1, (0, 250, 0), -1)
 
-        # check that blendshapes come through
-        # result.face_blendshapes is a list (one entry per detected face)
-        # each entry is a list of Category objects with .category_name and .score
-        if result.face_blendshapes:
-            first_blendshape = result.face_blendshapes[0][0]
-            print(f"{first_blendshape.category_name}: {first_blendshape.score:.3f}")
 
+        # {name: score} dict is used to get pspi score
+        if result.face_blendshapes:
+
+            # bs for blendshape not the other word
+            scores = {bs.category_name: bs.score for bs in result.face_blendshapes[0]}
+
+            pspi_score = calculate_pspi(scores)
+
+            print(f"PSPI score: {pspi_score:.3f}")
 
         # window for displaying video
         cv2.imshow("Facial Landmarks", frame)
